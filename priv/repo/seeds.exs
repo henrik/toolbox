6d516c307ca3442f0294965b4a4bffd7dcf397e8
Henrik Nyh <henrik@nyh.se>
2015-11-08 14:40:34 +0100

    Remove unused image

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,17 +1,10 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Toolbox.Repo.insert!(%SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
 
 if :dev == Mix.env do
   alias Toolbox.Package
+  alias Toolbox.Repo
 
   IO.puts "Creating dev data…"
 
