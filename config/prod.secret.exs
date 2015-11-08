1ff45ad9f7d4f6c7b647be5a1d9dfcfa3025fac6
Henrik Nyh <henrik@nyh.se>
2015-11-08 10:15:07 +0100

    Prepare for Dokku deploy
    Per https://gist.github.com/henrik/c70e32544e09c1a79841

---
+++ config/prod.secret.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :toolbox, Toolbox.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :toolbox, Toolbox.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("POSTGRESQL_URL"),
+  pool_size: 20
