f90e381ba1b5a7d5d34a8894c6f7550fd0e74220
Henrik Nyh <henrik@nyh.se>
2015-11-18 00:55:51 +0100

    Assign licenses to existing packages

---
+++ priv/repo/migrations/20151117231232_assign_licenses_to_existing_packages.exs
@@ -0,0 +1,33 @@
+defmodule Toolbox.Repo.Migrations.AssignLicensesToExistingPackages do
+  use Ecto.Migration
+  import Ecto.Query
+
+  def change do
+    # Migrations don't seem to run in the context of the Toolbox app, so we need to start this manually for the Hex client.
+    HTTPotion.start
+
+    update_from_page(1)
+  end
+
+  defp update_from_page(page) do
+    case get_packages_on_page(page) do
+      [] ->
+        # Empty page; stop.
+        :noop
+      packages ->
+        for package <- packages do
+          licenses = Dict.get(package["meta"], "licenses", [])
+
+          Toolbox.Repo.update_all(
+            from(p in Toolbox.Package, where: p.name==^package["name"]),
+            set: [licenses: licenses]
+          )
+        end
+        update_from_page(page + 1)
+    end
+  end
+
+  defp get_packages_on_page(page) do
+    HexClient.packages(page: page, sort: "updated_at")
+  end
+end
