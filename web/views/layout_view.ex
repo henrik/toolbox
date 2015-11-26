ba6a620d1ba38fa0566de5de293532f26abd9585
Henrik Nyh <henrik@nyh.se>
2015-11-26 20:26:41 +0100

    Style current page in menu
    Closes #17.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule Toolbox.LayoutView do
   use Toolbox.Web, :view
+
+  def menu_link(%Plug.Conn{request_path: req}, text, to: path) when req == path do
+    menu_link(text, to: path, extra_classes: "site-menu__link--current")
+  end
+
+  def menu_link(_, text, to: path) do
+    menu_link(text, to: path, extra_classes: "")
+  end
+
+  def menu_link(text, to: path, extra_classes: extra_classes) do
+    link(text, to: path, class: "site-menu__link #{extra_classes}")
+  end
 end
